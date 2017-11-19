90175769275cd84eebcf734a0ef19f062b271f67
root <root@ci_cd-devops.testlab.com>
2017-11-19 20:25:14 +0530

    First Changes in Python Program

---
+++ 2_Program_jenkins.py
@@ -0,0 +1,14 @@
+""" Sample Python code for uploading and Testing in Git"""
+
+variable= "hello world"
+
+## First Git Commit 
+
+for i in variable:
+	print (variable)
+
+## Updated after Git changes done !!
+
+# for j in range(10):
+	
+#  	print("This is to be Printed 10 Times !!")
